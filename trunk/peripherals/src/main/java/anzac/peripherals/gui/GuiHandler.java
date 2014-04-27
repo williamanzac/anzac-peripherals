6c68ffe080dd01a011064e0857c1fc89ea8f8721
anzacgames@gmail.com <anzacgames@gmail.com@4ab4ed43-a7a3-673f-5168-f632ee1cb340>
2014-04-27 10:44:39 +0000

    Updated how the api docs are generated. Changed how GuiHandler works. Some minor tweaks to the pom.

--- trunk/peripherals/src/main/java/anzac/peripherals/gui/GuiHandler.java
+++ trunk/peripherals/src/main/java/anzac/peripherals/gui/GuiHandler.java
@@ -1,26 +1,12 @@
 package anzac.peripherals.gui;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
-import anzac.peripherals.inventory.ChargeStationContainer;
-import anzac.peripherals.inventory.CraftingRouterContainer;
-import anzac.peripherals.inventory.FluidRouterContainer;
-import anzac.peripherals.inventory.FluidStorageContainer;
-import anzac.peripherals.inventory.ItemRouterContainer;
-import anzac.peripherals.inventory.ItemStorageContainer;
-import anzac.peripherals.inventory.RecipeStorageContainer;
-import anzac.peripherals.inventory.TeleporterContainer;
-import anzac.peripherals.inventory.WorkbenchContainer;
-import anzac.peripherals.tiles.ChargeStationTileEntity;
-import anzac.peripherals.tiles.CraftingRouterTileEntity;
-import anzac.peripherals.tiles.FluidRouterTileEntity;
-import anzac.peripherals.tiles.FluidStorageTileEntity;
-import anzac.peripherals.tiles.ItemRouterTileEntity;
-import anzac.peripherals.tiles.ItemStorageTileEntity;
-import anzac.peripherals.tiles.RecipeStorageTileEntity;
-import anzac.peripherals.tiles.TeleporterTileEntity;
-import anzac.peripherals.tiles.WorkbenchTileEntity;
 import cpw.mods.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
@@ -29,32 +15,19 @@ public class GuiHandler implements IGuiHandler {
 	public Object getServerGuiElement(final int ID, final EntityPlayer player, final World world, final int x,
 			final int y, final int z) {
 		final TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		if (tileEntity instanceof RecipeStorageTileEntity) {
-			return new RecipeStorageContainer(player.inventory, (RecipeStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof WorkbenchTileEntity) {
-			return new WorkbenchContainer(player.inventory, (WorkbenchTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof CraftingRouterTileEntity) {
-			return new CraftingRouterContainer(player.inventory, (CraftingRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ItemRouterTileEntity) {
-			return new ItemRouterContainer(player.inventory, (ItemRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof FluidRouterTileEntity) {
-			return new FluidRouterContainer(player.inventory, (FluidRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ItemStorageTileEntity) {
-			return new ItemStorageContainer(player.inventory, (ItemStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof FluidStorageTileEntity) {
-			return new FluidStorageContainer(player.inventory, (FluidStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ChargeStationTileEntity) {
-			return new ChargeStationContainer(player.inventory, (ChargeStationTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof TeleporterTileEntity) {
-			return new TeleporterContainer(player.inventory, (TeleporterTileEntity) tileEntity);
+		final Class<? extends TileEntity> tileClass = tileEntity.getClass();
+		final String containerClassName = tileClass.getName().replace("TitleEntity", "Container");
+		try {
+			final Class<?> containerClass = Class.forName("anzac.peripherals.inventory." + containerClassName);
+			final Constructor<?> constructor = containerClass.getConstructor(IInventory.class, tileClass);
+			return constructor.newInstance(player.inventory, tileEntity);
+		} catch (final ClassNotFoundException e) {
+		} catch (final NoSuchMethodException e) {
+		} catch (final SecurityException e) {
+		} catch (final InstantiationException e) {
+		} catch (final IllegalAccessException e) {
+		} catch (final IllegalArgumentException e) {
+		} catch (final InvocationTargetException e) {
 		}
 		return null;
 	}
@@ -63,32 +36,19 @@ public class GuiHandler implements IGuiHandler {
 	public Object getClientGuiElement(final int ID, final EntityPlayer player, final World world, final int x,
 			final int y, final int z) {
 		final TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
-		if (tileEntity instanceof RecipeStorageTileEntity) {
-			return new RecipeStorageGUI(player.inventory, (RecipeStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof WorkbenchTileEntity) {
-			return new WorkbenchGUI(player.inventory, (WorkbenchTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof CraftingRouterTileEntity) {
-			return new CraftingRouterGUI(player.inventory, (CraftingRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ItemRouterTileEntity) {
-			return new ItemRouterGUI(player.inventory, (ItemRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof FluidRouterTileEntity) {
-			return new FluidRouterGUI(player.inventory, (FluidRouterTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ItemStorageTileEntity) {
-			return new ItemStorageGUI(player.inventory, (ItemStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof FluidStorageTileEntity) {
-			return new FluidStorageGUI(player.inventory, (FluidStorageTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof ChargeStationTileEntity) {
-			return new ChargeStationGUI(player.inventory, (ChargeStationTileEntity) tileEntity);
-		}
-		if (tileEntity instanceof TeleporterTileEntity) {
-			return new TeleporterGUI(player.inventory, (TeleporterTileEntity) tileEntity);
+		final Class<? extends TileEntity> tileClass = tileEntity.getClass();
+		final String containerClassName = tileClass.getName().replace("TitleEntity", "GUI");
+		try {
+			final Class<?> containerClass = Class.forName("anzac.peripherals.gui." + containerClassName);
+			final Constructor<?> constructor = containerClass.getConstructor(IInventory.class, tileClass);
+			return constructor.newInstance(player.inventory, tileEntity);
+		} catch (final ClassNotFoundException e) {
+		} catch (final NoSuchMethodException e) {
+		} catch (final SecurityException e) {
+		} catch (final InstantiationException e) {
+		} catch (final IllegalAccessException e) {
+		} catch (final IllegalArgumentException e) {
+		} catch (final InvocationTargetException e) {
 		}
 		return null;
 	}
